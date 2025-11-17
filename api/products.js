7faa60a719d84d059a7803b0e11ad6de13d13e83
Cora Ciuraneta <cora.ciuraneta@gmail.com>
2025-11-17 17:02:10 +0100

    api

--- api/products.js
+++ api/products.js
@@ -1,4 +1,4 @@
-import data from "../products.json" assert { type: "json" };
+import data from "../data/products.json" assert { type: "json" };
 
 export default function handler(req, res) {
   res.status(200).json(data.products);
